cd81371592775f0b67af54d5a521c19b0ca500bf
Mark Du <dulongjiebackup@gmail.com>
2024-04-10 10:58:13 -0400

    add a github action

--- src/app/(main)/(pages)/workflows/page.tsx
+++ src/app/(main)/(pages)/workflows/page.tsx
@@ -1,10 +1,24 @@
 import { Metadata } from "next"
 import React from 'react'
 export const metadata:Metadata = {
-    title:"Dashboard"
+    title:"Workflow"
 }
-export default function DashBoardPage(){
-    return (
-        <div>Page</div>
-    )
+
+// import WorkflowButton from './_components/workflow-button'
+// import Workflows from './_components'
+
+type Props = {}
+
+const Page = (props: Props) => {
+  return (
+    <div className="flex flex-col relative">
+      <h1 className="text-4xl sticky top-0 z-[10] p-6 bg-background/50 backdrop-blur-lg flex items-center border-b justify-between">
+        Workflows
+        {/* <WorkflowButton /> */}
+      </h1>
+      {/* <Workflows /> */}
+    </div>
+  )
 }
+
+export default Page
